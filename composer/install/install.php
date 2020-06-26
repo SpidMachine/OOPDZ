9249e4d9414c174f51be89fdc693d3eeb27dfc8e
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-06-26 19:04:50 +0300

    dz

---
+++ composer/install/install.php
@@ -0,0 +1,16 @@
+<?php
+
+require "../vendor/autoload.php";
+
+use TexLab\MyDB\Runner;
+use TexLab\MyDB\DB;
+
+$runner = new Runner(DB::Link([
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+]));
+
+foreach (explode(";", file_get_contents('guests_book.sql')) as $value) {
+    $runner->runSQL($value . ";");
+}
